863c273ca49390da8f30db945f30f3c4ad78cc06
Matthew Slone <Matthew.Slone@outlook.com>
2018-05-16 14:28:31 -0400

    Toggle password visibility in the user and professional tab pane.

---
+++ code/Fixit.WebUI/Scripts/Login.PasswordToggle.js
@@ -0,0 +1,12 @@
+﻿function PasswordToggle() {
+
+    var x = document.getElementById("myTypedPassword");
+
+    if (x.type == "password") {
+        x.type = "text";
+    }
+    else {
+        x.type = "password";
+    }
+
+}
